ae970d015c01a951c2d24e4b0a076c9d3e8814be
Greg Brown <gk_brown@icloud.com>
2022-12-31 07:41:01 -0500

    Complete periodic table example.

--- sierra-test/src/main/java/org/httprpc/sierra/PeriodicTableTest.java
+++ sierra-test/src/main/java/org/httprpc/sierra/PeriodicTableTest.java
@@ -28,6 +28,7 @@ import java.awt.Font;
 
 import static org.httprpc.sierra.UIBuilder.cell;
 import static org.httprpc.sierra.UIBuilder.column;
+import static org.httprpc.sierra.UIBuilder.glue;
 import static org.httprpc.sierra.UIBuilder.row;
 
 public class PeriodicTableTest extends JFrame implements Runnable {
@@ -88,6 +89,8 @@ public class PeriodicTableTest extends JFrame implements Runnable {
 
     private static class KeyPanel extends ColumnPanel {
         KeyPanel(String text, Group group) {
+            setPreferredSize(new Dimension(84, 36));
+
             var labelFont = javax.swing.UIManager.getDefaults().getFont("Label.font");
 
             var textPane = new TextPane(text, true);
@@ -95,9 +98,9 @@ public class PeriodicTableTest extends JFrame implements Runnable {
             textPane.setForeground(new Color(0x0c47a7));
             textPane.setHorizontalAlignment(HorizontalAlignment.CENTER);
             textPane.setVerticalAlignment(VerticalAlignment.CENTER);
-            textPane.setFont(labelFont.deriveFont(Font.PLAIN, 12));
+            textPane.setFont(labelFont.deriveFont(Font.PLAIN, 10));
 
-            add(textPane);
+            add(textPane, 1.0);
 
             setOpaque(true);
             setBackground(group.color);
@@ -133,7 +136,7 @@ public class PeriodicTableTest extends JFrame implements Runnable {
                 cell(new ElementPanel()),
                 cell(new ElementPanel()),
                 cell(new ElementPanel()),
-                cell(new ElementPanel(1, "He", Group.NOBLE_GAS))
+                cell(new ElementPanel(2, "He", Group.NOBLE_GAS))
             ),
             row(4,
                 cell(new ElementPanel(3, "Li", Group.ALKALI_METAL)),
@@ -155,18 +158,160 @@ public class PeriodicTableTest extends JFrame implements Runnable {
                 cell(new ElementPanel(9, "F", Group.REACTIVE_NONMETAL)),
                 cell(new ElementPanel(10, "Ne", Group.NOBLE_GAS))
             ),
+            row(4,
+                cell(new ElementPanel(11, "Na", Group.ALKALI_METAL)),
+                cell(new ElementPanel(12, "Mg", Group.ALKALINE_EARTH_METAL)),
+                cell(new ElementPanel()),
+                cell(new ElementPanel()),
+                cell(new ElementPanel()),
+                cell(new ElementPanel()),
+                cell(new ElementPanel()),
+                cell(new ElementPanel()),
+                cell(new ElementPanel()),
+                cell(new ElementPanel()),
+                cell(new ElementPanel()),
+                cell(new ElementPanel()),
+                cell(new ElementPanel(13, "Al", Group.POST_TRANSITION_METAL)),
+                cell(new ElementPanel(14, "Si", Group.METALLOID)),
+                cell(new ElementPanel(15, "P", Group.REACTIVE_NONMETAL)),
+                cell(new ElementPanel(16, "S", Group.REACTIVE_NONMETAL)),
+                cell(new ElementPanel(17, "Cl", Group.REACTIVE_NONMETAL)),
+                cell(new ElementPanel(18, "Ar", Group.NOBLE_GAS))
+            ),
+            row(4,
+                cell(new ElementPanel(19, "K", Group.ALKALI_METAL)),
+                cell(new ElementPanel(20, "Ca", Group.ALKALINE_EARTH_METAL)),
+                cell(new ElementPanel(21, "Sc", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(22, "Ti", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(23, "V", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(24, "Cr", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(25, "Mn", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(26, "Fe", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(27, "Co", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(28, "Ni", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(29, "Cu", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(30, "Zn", Group.POST_TRANSITION_METAL)),
+                cell(new ElementPanel(31, "Ga", Group.POST_TRANSITION_METAL)),
+                cell(new ElementPanel(32, "Ge", Group.METALLOID)),
+                cell(new ElementPanel(33, "As", Group.METALLOID)),
+                cell(new ElementPanel(34, "Se", Group.REACTIVE_NONMETAL)),
+                cell(new ElementPanel(35, "Br", Group.REACTIVE_NONMETAL)),
+                cell(new ElementPanel(36, "Kr", Group.NOBLE_GAS))
+            ),
+            row(4,
+                cell(new ElementPanel(37, "Rb", Group.ALKALI_METAL)),
+                cell(new ElementPanel(38, "Sr", Group.ALKALINE_EARTH_METAL)),
+                cell(new ElementPanel(39, "Y", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(40, "Zr", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(41, "Nb", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(42, "Mo", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(43, "Tc", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(44, "Ru", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(45, "Rh", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(46, "Pd", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(47, "Ag", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(48, "Cd", Group.POST_TRANSITION_METAL)),
+                cell(new ElementPanel(49, "In", Group.POST_TRANSITION_METAL)),
+                cell(new ElementPanel(50, "Sn", Group.POST_TRANSITION_METAL)),
+                cell(new ElementPanel(51, "Sb", Group.METALLOID)),
+                cell(new ElementPanel(52, "Te", Group.METALLOID)),
+                cell(new ElementPanel(53, "I", Group.REACTIVE_NONMETAL)),
+                cell(new ElementPanel(54, "Xe", Group.NOBLE_GAS))
+            ),
+            row(4,
+                cell(new ElementPanel(55, "Cs", Group.ALKALI_METAL)),
+                cell(new ElementPanel(56, "Ba", Group.ALKALINE_EARTH_METAL)),
+                cell(new ElementPanel(57, "La", Group.LANTHANIDE)),
+                cell(new ElementPanel(72, "Hf", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(73, "Ta", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(74, "W", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(75, "Re", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(76, "Os", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(77, "Ir", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(78, "Pt", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(79, "Au", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(80, "Hg", Group.POST_TRANSITION_METAL)),
+                cell(new ElementPanel(81, "Tl", Group.POST_TRANSITION_METAL)),
+                cell(new ElementPanel(82, "Pb", Group.POST_TRANSITION_METAL)),
+                cell(new ElementPanel(83, "Bi", Group.POST_TRANSITION_METAL)),
+                cell(new ElementPanel(84, "Po", Group.POST_TRANSITION_METAL)),
+                cell(new ElementPanel(85, "At", Group.METALLOID)),
+                cell(new ElementPanel(86, "Rn", Group.NOBLE_GAS))
+            ),
+            row(4,
+                cell(new ElementPanel(87, "Fr", Group.ALKALI_METAL)),
+                cell(new ElementPanel(88, "Ra", Group.ALKALINE_EARTH_METAL)),
+                cell(new ElementPanel(89, "Ac", Group.ACTINIDE)),
+                cell(new ElementPanel(104, "Rf", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(105, "Db", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(106, "Sg", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(107, "Bh", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(108, "Hs", Group.TRANSITION_METAL)),
+                cell(new ElementPanel(109, "Mt", Group.UNKNOWN)),
+                cell(new ElementPanel(110, "Ds", Group.UNKNOWN)),
+                cell(new ElementPanel(111, "Rg", Group.UNKNOWN)),
+                cell(new ElementPanel(112, "Cn", Group.POST_TRANSITION_METAL)),
+                cell(new ElementPanel(113, "Nh", Group.UNKNOWN)),
+                cell(new ElementPanel(114, "Fl", Group.UNKNOWN)),
+                cell(new ElementPanel(115, "Mc", Group.UNKNOWN)),
+                cell(new ElementPanel(116, "Lv", Group.UNKNOWN)),
+                cell(new ElementPanel(117, "Ts", Group.UNKNOWN)),
+                cell(new ElementPanel(118, "Og", Group.UNKNOWN))
+            ),
+            row(4,
+                cell(new ElementPanel()),
+                cell(new ElementPanel()),
+                cell(new ElementPanel()),
+                cell(new ElementPanel(58, "Ce", Group.LANTHANIDE)),
+                cell(new ElementPanel(59, "Pr", Group.LANTHANIDE)),
+                cell(new ElementPanel(60, "Nd", Group.LANTHANIDE)),
+                cell(new ElementPanel(61, "Pm", Group.LANTHANIDE)),
+                cell(new ElementPanel(62, "Sm", Group.LANTHANIDE)),
+                cell(new ElementPanel(63, "Eu", Group.LANTHANIDE)),
+                cell(new ElementPanel(64, "Gd", Group.LANTHANIDE)),
+                cell(new ElementPanel(65, "Tb", Group.LANTHANIDE)),
+                cell(new ElementPanel(66, "Dy", Group.LANTHANIDE)),
+                cell(new ElementPanel(67, "Ho", Group.LANTHANIDE)),
+                cell(new ElementPanel(68, "Er", Group.LANTHANIDE)),
+                cell(new ElementPanel(69, "Tm", Group.LANTHANIDE)),
+                cell(new ElementPanel(70, "Yb", Group.LANTHANIDE)),
+                cell(new ElementPanel(71, "Lu", Group.LANTHANIDE)),
+                cell(new ElementPanel())
+            ),
+            row(4,
+                cell(new ElementPanel()),
+                cell(new ElementPanel()),
+                cell(new ElementPanel()),
+                cell(new ElementPanel(90, "Th", Group.ACTINIDE)),
+                cell(new ElementPanel(91, "Pa", Group.ACTINIDE)),
+                cell(new ElementPanel(92, "U", Group.ACTINIDE)),
+                cell(new ElementPanel(93, "Np", Group.ACTINIDE)),
+                cell(new ElementPanel(94, "Pu", Group.ACTINIDE)),
+                cell(new ElementPanel(95, "Am", Group.ACTINIDE)),
+                cell(new ElementPanel(96, "Cm", Group.ACTINIDE)),
+                cell(new ElementPanel(97, "Bk", Group.ACTINIDE)),
+                cell(new ElementPanel(98, "Cf", Group.ACTINIDE)),
+                cell(new ElementPanel(99, "Es", Group.ACTINIDE)),
+                cell(new ElementPanel(100, "Fm", Group.ACTINIDE)),
+                cell(new ElementPanel(101, "Md", Group.ACTINIDE)),
+                cell(new ElementPanel(102, "No", Group.ACTINIDE)),
+                cell(new ElementPanel(103, "Lr", Group.ACTINIDE)),
+                cell(new ElementPanel())
+            ),
             cell(new JSeparator()),
             row(8,
-                cell(new KeyPanel("Alkali metal", Group.ALKALI_METAL)).weightBy(1),
-                cell(new KeyPanel("Alkaline earth metal", Group.ALKALINE_EARTH_METAL)).weightBy(1),
-                cell(new KeyPanel("Lanthanide", Group.LANTHANIDE)).weightBy(1),
-                cell(new KeyPanel("Actinide", Group.ACTINIDE)).weightBy(1),
-                cell(new KeyPanel("Transition metal", Group.TRANSITION_METAL)).weightBy(1),
-                cell(new KeyPanel("Post-transition metal", Group.POST_TRANSITION_METAL)).weightBy(1),
-                cell(new KeyPanel("Metalloid", Group.METALLOID)).weightBy(1),
-                cell(new KeyPanel("Reactive nonmetal", Group.REACTIVE_NONMETAL)).weightBy(1),
-                cell(new KeyPanel("Noble gas", Group.NOBLE_GAS)).weightBy(1),
-                cell(new KeyPanel("Unknown", Group.UNKNOWN)).weightBy(1)
+                glue(),
+                cell(new KeyPanel("Alkali metal", Group.ALKALI_METAL)),
+                cell(new KeyPanel("Alkaline earth metal", Group.ALKALINE_EARTH_METAL)),
+                cell(new KeyPanel("Lanthanide", Group.LANTHANIDE)),
+                cell(new KeyPanel("Actinide", Group.ACTINIDE)),
+                cell(new KeyPanel("Transition metal", Group.TRANSITION_METAL)),
+                cell(new KeyPanel("Post-transition metal", Group.POST_TRANSITION_METAL)),
+                cell(new KeyPanel("Metalloid", Group.METALLOID)),
+                cell(new KeyPanel("Reactive nonmetal", Group.REACTIVE_NONMETAL)),
+                cell(new KeyPanel("Noble gas", Group.NOBLE_GAS)),
+                cell(new KeyPanel("Unknown", Group.UNKNOWN)),
+                glue()
             )
         ).with(columnPanel -> columnPanel.setBorder(new EmptyBorder(8, 8, 8, 8))).getComponent());
 
