2dc40f41fe4b45c07665fb2148a285ffd191b225
Greg Brown <gk_brown@icloud.com>
2025-01-01 14:53:19 -0500

    Stub out Tiingo example.

---
+++ sierra-test/src/main/java/org/httprpc/sierra/test/TiingoTest.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.httprpc.sierra.test;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import org.httprpc.sierra.UILoader;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import java.util.ResourceBundle;
+
+public class TiingoTest extends JFrame implements Runnable {
+    private static final ResourceBundle resourceBundle = ResourceBundle.getBundle(TiingoTest.class.getName());
+
+    private TiingoTest() {
+        super(resourceBundle.getString("title"));
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void run() {
+        setContentPane(UILoader.load(this, "tiingo-test.xml", resourceBundle));
+
+        setSize(720, 360);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        FlatLightLaf.setup();
+
+        SwingUtilities.invokeLater(new TiingoTest());
+    }
+}
