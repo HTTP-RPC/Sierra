5ec79446da9f37c4c33e9b73ddd93a4a3617b991
Greg Brown <gk_brown@icloud.com>
2025-10-13 12:47:29 -0400

    Update examples.

---
+++ sierra-test/src/main/java/org/httprpc/sierra/test/SliderTest.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.httprpc.sierra.test;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import org.httprpc.sierra.UILoader;
+
+import javax.swing.JFrame;
+import javax.swing.JProgressBar;
+import javax.swing.JSlider;
+import javax.swing.SwingUtilities;
+
+public class SliderTest extends JFrame implements Runnable {
+    private JSlider slider = null;
+    private JProgressBar progressBar = null;
+
+    private SliderTest() {
+        super("Slider Test");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void run() {
+        setContentPane(UILoader.load(this, "SliderTest.xml"));
+
+        slider.addChangeListener(event -> updateProgress());
+
+        slider.setValue(50);
+
+        updateProgress();
+
+        pack();
+        setVisible(true);
+    }
+
+    private void updateProgress() {
+        progressBar.setValue(slider.getValue());
+    }
+
+    public static void main(String[] args) {
+        FlatLightLaf.setup();
+
+        SwingUtilities.invokeLater(new SliderTest());
+    }
+}
