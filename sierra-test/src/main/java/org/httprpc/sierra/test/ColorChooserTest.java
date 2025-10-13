49d0ae708029aa7407485ff56fbbd4f885a6629c
Greg Brown <gk_brown@icloud.com>
2025-10-13 12:57:58 -0400

    Stub out color chooser test.

---
+++ sierra-test/src/main/java/org/httprpc/sierra/test/ColorChooserTest.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.httprpc.sierra.test;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import org.httprpc.sierra.UILoader;
+
+import javax.swing.JColorChooser;
+import javax.swing.JFrame;
+import javax.swing.JTree;
+import javax.swing.SwingUtilities;
+
+public class ColorChooserTest extends JFrame implements Runnable {
+    private JColorChooser colorChooser = null;
+    private JTree tree = null;
+
+    private ColorChooserTest() {
+        super("Color Chooser Test");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void run() {
+        setContentPane(UILoader.load(this, "ColorChooserTest.xml"));
+
+        setSize(1024, 480);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        FlatLightLaf.setup();
+
+        SwingUtilities.invokeLater(new ColorChooserTest());
+    }
+}
