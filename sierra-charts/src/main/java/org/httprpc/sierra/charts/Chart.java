76d9e6da1e617b84e6a8b357c5d4413bc83d5c98
Greg Brown <gk_brown@icloud.com>
2025-12-08 09:36:07 -0500

    Update charts.

--- sierra-charts/src/main/java/org/httprpc/sierra/charts/Chart.java
+++ sierra-charts/src/main/java/org/httprpc/sierra/charts/Chart.java
@@ -16,28 +16,72 @@ package org.httprpc.sierra.charts;
 
 import java.awt.Graphics2D;
 
+/**
+ * Abstract base class for charts.
+ */
 public abstract class Chart {
+    /**
+     * Chart orientation options.
+     */
     public enum Orientation {
+        /**
+         * Horizontal orientation.
+         */
         HORIZONTAL,
+
+        /**
+         * Vertical orientation.
+         */
         VERTICAL
     }
 
+    /**
+     * Chart axis options.
+     */
     public enum Axis {
+        /**
+         * Leading axis.
+         */
         LEADING,
+
+        /**
+         * Trailing axis.
+         */
         TRAILING
     }
 
     private int width = 320;
     private int height = 240;
 
+    /**
+     * Returns the chart's width.
+     *
+     * @return
+     * The chart width.
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     * Returns the chart's height.
+     *
+     * @return
+     * The chart height.
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * Sets the chart's size.
+     *
+     * @param width
+     * The chart width.
+     *
+     * @param height
+     * The chart height.
+     */
     public void setSize(int width, int height) {
         if (width < 0 || height < 0) {
             throw new IllegalArgumentException();
@@ -47,5 +91,11 @@ public abstract class Chart {
         this.height = height;
     }
 
+    /**
+     * Draws the chart.
+     *
+     * @param graphics
+     * The graphics context in which the chart will be drawn.
+     */
     public abstract void draw(Graphics2D graphics);
 }
